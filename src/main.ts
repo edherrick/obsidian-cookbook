9bb5be3af3229023a937f3079e0c6d6c37cd056d
natbetts31 <natbetts31@gmail.com>
2026-02-01 11:11:08 -0700

    test

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { getRecipes } from "utils/getRecipes";
 
 export default class CookbookPlugin extends Plugin {
 	settings: CookbookSettings;
-
+	// test change from nat
 	async onload() {
 		await this.loadSettings();
 
